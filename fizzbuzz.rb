50bf5dc542ca6aaba49ae7fd2d4c64adc45a01bd
Sonya Gould <sonyavgould@gmail.com>
2020-05-21 01:24:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 5 == 0 and int % 3 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 5 == 0 and int % 3 == 0
-  "FizzBuzz"
 else
   puts int
 end
